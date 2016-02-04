7e63d2d8abc321c4ea6819a82e6c2763a7181db6
KarimGuesmi <karim_poste@yahoo.fr>
2016-02-04 22:11:28 +0100

    Create Briefs.java
    This file will concat all Keys from (BriefsKeys.txt) with (TranslationKey.txt) and form a new jsonFile (Briefs.JSON) which contains common words.

---
+++ Briefs.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.json.simple.JSONObject;
+
+
+public class Briefs {
+
+	public static void main(String[] args) throws IOException {
+		JSONObject obj = new JSONObject();
+		
+		FileInputStream fis1 =new FileInputStream("BriefsKeys.txt");
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(fis1));
+		
+		FileInputStream fis2 =new FileInputStream("BriefTranslation.txt");
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
+		
+		String strLine1;
+		String strLine2;
+		
+		while(((strLine1 = br1.readLine()) != null) && ((strLine2 = br2.readLine()) != null)){
+			System.out.print(strLine1);
+			System.out.print(" ");
+			System.out.println(strLine2);
+			obj.put(strLine1, strLine2);
+		}
+		
+		FileWriter fw = new FileWriter("Briefs.json");
+		fw.write(obj.toJSONString());
+		
+		fw.flush();
+		fw.close();
+		
+		
+	}
+
+}
