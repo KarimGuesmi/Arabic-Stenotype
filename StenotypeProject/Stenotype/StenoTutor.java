18b71cd1cb0431e371b5d6da2e5dde4a9d11b73d
KarimGuesmi <karim_poste@yahoo.fr>
2016-07-03 21:27:17 +0200

    Update StenoTutor.java

--- StenotypeProject/Stenotype/StenoTutor.java
+++ StenotypeProject/Stenotype/StenoTutor.java
@@ -64,6 +64,67 @@ public class StenoTutor {
 	Stroke previousStroke = new Stroke();
 	boolean ctrlKeyRelease=false;
 	boolean tabKeyReleased=false;
-	boolean debud =false;
+	boolean debug =false;
 
+	
+	public void setup(){
+		readSessionConfig();
+		findPloverLog();
+		lesDictionaryFilePath = "/lessons/" + lessonName + ".les";
+		chdDictionaryFilePath = "/lessons/" + lessonName + ".chd";
+		blkDictionaryFilePath = "/lessons/" + lessonName + ".blk";
+		dictionary = utils.readDictionary(lesDictionaryFilePath, chdDictionaryFilePath, debug);
+		wordsBlackList = utils.readBackList(blkDictionaryFilePath);
+		applyStartBacklist();
+		
+		// Initialize word stats
+		for(int i=0; i<dictionary.size();i++){
+			wordStats.add(new WordStats(wordStartAvgWpm, wordAvgSamples));
+		}
+		
+		// initialise target line buffer and set the next word index
+		nextWordBuffer =  new NextWordsBuffer(700-180);
+		currentWordIndex = nextWordBuffer.getCurrentWordIndex();
+		keyBoard = new KeyBoard(50, 300);
+		Stroke stroke = new Stroke();
+		showTextInfo(stroke);
+	}
+	
+	// Draw cycle
+	public void draw(){
+		if(ctrlKeyRelease){
+			blackListCurrentWord();
+		}
+		//If tab Key released, pause & resume this session
+		// LINE 252
+	}
+
+
+	private void blackListCurrentWord() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void showTextInfo(Stroke stroke) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void applyStartBacklist() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void findPloverLog() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void readSessionConfig() {
+		// TODO Auto-generated method stub
+		
+	}
 }
