555bf5bcdb830d26222f9e754abaf3962d7de0d9
KarimGuesmi <karim_poste@yahoo.fr>
2016-08-31 20:08:29 +0200

    Create main.java

---
+++ Steno-Genetic/classes/main.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Individual indiv = new Individual();
+		String file="briefs.txt";
+		indiv.wordFromBook(file);
+		
+	}
+
+}
