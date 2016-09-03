b9d87ae3f00f66e773e776f234cf713d94b6f693
KarimGuesmi <karim_poste@yahoo.fr>
2016-09-03 12:20:35 +0200

    Update main.java

--- Steno-Genetic/classes/main.java
+++ Steno-Genetic/classes/main.java
@@ -1,15 +1,31 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
+		/*
+			Creating the new Individual
+			Individual = caracter from the word got from the book
+		*/
 		Individual indiv = new Individual();
 		String file="briefs.txt";
-		indiv.wordFromBook(file);
+		ArrayList<String> list = new ArrayList<String>(); 
+		list = indiv.wordsFromBook(file);
 		
+		ArrayList<Character> listChars = new ArrayList<Character>();
+		ArrayList<ArrayList<Character>>listOfListsOfWords = new ArrayList<ArrayList<Character>>();
+		
+		// Display the list of all words from the book
+		// for each word , decompose the word to set of letters and put them into a new List
+		for(int i=0; i<list.size();i++){
+			System.out.println(list.get(i));
+		}
+		System.out.println(list);
+		
+		listChars = indiv.decomposeWord(list.get(0));
+		System.out.println(listChars);
+	
 	}
-
 }
