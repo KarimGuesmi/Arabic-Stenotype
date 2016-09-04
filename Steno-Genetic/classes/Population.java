f57fff722f26ef103e07ebb2717ad6fdfa4ac6d5
KarimGuesmi <karim_poste@yahoo.fr>
2016-09-04 11:59:29 +0200

    Update Population.java

--- Steno-Genetic/classes/Population.java
+++ Steno-Genetic/classes/Population.java
@@ -1,19 +1,57 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Population {
 	Individual[] individuals;
 
 	// List Of lettes 
+	List<Character> lettersList = new ArrayList<Character>();
 	
 	// List of Keys
+	List<String> keysList = new ArrayList<String>();
 	
-	public Population(int populationSize) {
+	// value => translation  &&  key => character    
+	private HashMap<String, String> map = new HashMap<String, String>();
+	
+	//Constructor
+	public Population() {
 		super();
-		individuals = new Individual[populationSize];
 	}
 
 	public int size() {
 		return individuals.length;
+	}
+	
+	//some examples of keys => translation 
+	public void addPopulation(){
+		map.put("S", "");
+		map.put("T", "");
+		map.put("K", "");
+		map.put("P", "");
+		map.put("W", "");
+		map.put("H", "");
+		map.put("R", "");
+		map.put("A", "");
+		map.put("O", "");
+		map.put("*", "");
+		map.put("E", "");
+		map.put("U", "");
+		map.put("F", "");
+		map.put("R", "");
+		map.put("P", "");
+		map.put("B", "");
+		map.put("L", "");
+		map.put("G", "");
+		map.put("T", "");
+		map.put("S", "");
+		map.put("D", "");
+		map.put("Z", "");
+		
+		
+		
 	}
 	
 	
