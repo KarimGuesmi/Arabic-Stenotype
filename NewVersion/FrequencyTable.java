d8b95e58abc14abd3ad052dd975a01b1b5fc2439
KarimGuesmi <karim_poste@yahoo.fr>
2016-12-08 02:12:49 -1200

    Create FrequencyTable.java
    This file is taking each word from the text file
    and calculate the word frequency by providing the number of appearance of each word and also the percentage comparing to all words.

---
+++ NewVersion/FrequencyTable.java
@@ -0,0 +1,105 @@
+package Preparation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FrequencyTable {
+	private String filename;
+	private LinkedList<String> keywordsList;
+	private TreeMap<String, Integer> freqMap;
+
+	FrequencyTable(String filename) {
+		this.filename = filename;
+		freqMap = new TreeMap<String, Integer>();
+		keywordsList = new LinkedList<String>();
+	}
+
+	public void readWords() {
+		Pattern pattern = Pattern.compile("\\W+");
+		try {
+
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				// split a line by spaces so we get words
+				String[] words = strLine.split("[ ]+");
+				for (String word : words) {
+					// remove all symbols except underscore
+					Matcher mat = pattern.matcher(word);
+					word = mat.replaceAll("");
+					// add words to the list
+					keywordsList.add(word.toLowerCase());
+				}
+			}
+
+			br.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void countWords() {
+		int count = 1;
+		String word = "";
+		for (int i = 0; i < keywordsList.size(); i++) {
+			word = keywordsList.get(i);
+			for (int j = i + 1; j < keywordsList.size(); j++) {
+				if (word.equals(keywordsList.get(j))) {
+					count++; // increase the number of duplicate words
+				}
+			}
+			// add the word and its frequency to the TreeMap
+			addToMap(word, count);
+			// reset the count variable
+			count = 1;
+		}
+
+	}
+
+	public void addToMap(String word, int count) {
+		// place keyword and its frequency in TreeMap
+		if (!freqMap.containsKey(word) && word.length() >= 1) {
+			freqMap.put(word, count);
+		}
+
+	}
+
+	public void showResult() {
+		Set<String> keys = freqMap.keySet();
+		int numWord = keys.size();
+		Iterator<String> iterator = keys.iterator();
+		while (iterator.hasNext()) {
+			String word = iterator.next();
+			int count = freqMap.get(word);
+			System.out.format("%-20s%-5d%-2s\n", word, count, 100 * count
+					/ numWord + "%");
+		}
+
+	}
+
+	public void processCounting() {
+		Thread backprocess = new Thread() {
+			public void run() {
+				readWords();
+				countWords();
+				showResult();
+			}
+		};
+		backprocess.start();
+	}
+
+	public static void main(String[] args) {
+		
+			FrequencyTable FT = new FrequencyTable("Bookk.txt");
+			FT.processCounting();
+		
+	}
+}
