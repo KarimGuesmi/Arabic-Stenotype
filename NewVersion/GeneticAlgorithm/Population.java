3a5d753d30b72214cb0c111041975290de380a82
KarimGuesmi <karim_poste@yahoo.fr>
2017-03-06 10:01:25 -1200

    Update Population.java

--- NewVersion/GeneticAlgorithm/Population.java
+++ NewVersion/GeneticAlgorithm/Population.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
 import preparation.SplitingOnlySentences;
 import preparation.SplitingText;
 
@@ -14,9 +16,8 @@ public class Population {
 	private SplitingOnlySentences sp = new SplitingOnlySentences();
 	private ArrayList<ArrayList<String>> listOfWordsList = new ArrayList<ArrayList<String>>();
 	private ArrayList<ArrayList<String>> listOfWordLetters = new ArrayList<ArrayList<String>>();
-	private ArrayList<ArrayList<ArrayList<String>>> listOfListWordLetters = new ArrayList<ArrayList<ArrayList<String>>>();
+	private ArrayList<ArrayList<String>> listOfLettersStrokes = new ArrayList<ArrayList<String>>();
 	
-
 	
 	public void displayWords(String sentence) {
 		String [] words = sentence.split(" ");
@@ -24,8 +25,26 @@ public class Population {
 		listOfWordsList.add(wordList);
 		setListOfWordsList(listOfWordsList);
 		//System.out.println(wordList);
-		//System.out.println(ListOfWordsList);
-		
+		//System.out.println(ListOfWordsList);		
+	}
+	
+	/*
+	 * Display the list of letters 
+	 */
+	public void displayLetters(ArrayList<String> wordsList) {
+		for(int k=0; k<wordsList.size();k++){
+			String word = wordsList.get(k);
+			ArrayList<String> letterList = new ArrayList<String>();
+			for(int i=0; i< word.length();i++){
+				letterList.add(word.substring(i, i+1));
+			}
+			listOfWordLetters.add(letterList);
+			setListOfWordLetters(listOfWordLetters);
+			
+			word=null;
+			letterList=null;
+		}	
+		System.out.println(listOfWordLetters);
 	}
 	
 	/*
@@ -43,11 +62,11 @@ public class Population {
 	public void setListOfWordLetters(ArrayList<ArrayList<String>> listOfWordLetters) {
 		this.listOfWordLetters = listOfWordLetters;
 	}
-	public ArrayList<ArrayList<ArrayList<String>>> getListOfListWordLetters() {
-		return listOfListWordLetters;
+	public ArrayList<ArrayList<String>> getListOfLettersStrokes() {
+		return listOfLettersStrokes;
 	}
-	public void setListOfListWordLetters(ArrayList<ArrayList<ArrayList<String>>> listOfListWordLetters) {
-		this.listOfListWordLetters = listOfListWordLetters;
+	public void setListOfLettersStrokes(ArrayList<ArrayList<String>> listOfLettersStrokes) {
+		this.listOfLettersStrokes = listOfLettersStrokes;
 	}
 
 	/*
@@ -62,31 +81,26 @@ public class Population {
 		for(int i=0; i<pop.sp.getSentencesList().size();i++){
 			pop.displayWords(pop.sp.getSentencesList().get(i));
 		}
+		pop.displayLetters(pop.listOfWordsList.get(0));
 		
-		//System.out.println(pop.listOfWordsList.get(0));
-		for(int i=0;i<pop.listOfWordsList.size();i++){
-			pop.displayLetters(pop.listOfWordsList.get(i));
-		}
-		System.out.println(pop.listOfListWordLetters.get(0));
+		System.out.println("**********************************************");
+		System.out.println("Generation 1 :");
+		//System.out.println(pop.listOfWordLetters.get(0).get(1));
+		pop.affectStrokes(pop.listOfWordLetters.get(0));
+		
 	}
 
-	public void displayLetters(ArrayList<String> wordsList) {
-		for(int k=0; k<wordsList.size();k++){
-			String word = wordsList.get(k);
-			ArrayList<String> letterList = new ArrayList<String>();
-			for(int i=0; i< word.length();i++){
-				letterList.add(word.substring(i, i+1));
-			}
-			listOfWordLetters.add(letterList);
-			setListOfWordLetters(listOfWordLetters);
-			//System.out.println(letterList);
-			word=null;
-			letterList=null;
-		}
-			listOfListWordLetters.add(listOfWordLetters);
-			//System.out.println(getListOfWordLetters());
-			//System.out.println(listOfListWordLetters);
+	public void affectStrokes(ArrayList<String> listLetters) {
 		
+		for(int i=0; i<listLetters.size();i++ ){
+			
+		}
+		if(en.getHm().containsValue(listLetters.get(1))){
+			System.out.println(true);
+			System.out.println(en.getHm());
+		}else 
+			System.out.println(false);
 	}
-	
+
+
 }
