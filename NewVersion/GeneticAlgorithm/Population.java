00225df07e2bd3ed4fd8facf7716cffa9d9e936b
KarimGuesmi <karim_poste@yahoo.fr>
2017-03-05 21:53:41 -1200

    Update Population.java

--- NewVersion/GeneticAlgorithm/Population.java
+++ NewVersion/GeneticAlgorithm/Population.java
@@ -1,21 +1,42 @@
 package GeneticAlgorithm;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.BreakIterator;
-import java.util.ArrayList;
-import java.util.Locale;
 
-import preparation.SplitingOnlySentencesForEachParagraph;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import preparation.SplitingOnlySentences;
 import preparation.SplitingText;
 
 public class Population {
 
 	private Entity en = new Entity();
-	
-	SplitingOnlySentencesForEachParagraph sp = new SplitingOnlySentencesForEachParagraph();
+	private SplitingOnlySentences sp = new SplitingOnlySentences();
+	private ArrayList<ArrayList<String>> ListOfWordsList = new ArrayList<ArrayList<String>>();
 
+	
+	public void displayWords(String sentence) {
+		String [] words = sentence.split(" ");
+		ArrayList<String> wordList = new ArrayList<String>(Arrays.asList(words));
+		ListOfWordsList.add(wordList);
+		setListOfWordsList(ListOfWordsList);
+		//System.out.println(wordList);
+		//System.out.println(ListOfWordsList);
+		
+	}
+	
+	/*
+	 * Getters && Setters
+	 */
+	public ArrayList<ArrayList<String>> getListOfWordsList() {
+		return ListOfWordsList;
+	}
+	public void setListOfWordsList(ArrayList<ArrayList<String>> listOfWordsList) {
+		ListOfWordsList = listOfWordsList;
+	}
+	
+	
+	
 	/*
 	 * Main program for the TEST
 	 */
@@ -24,6 +45,11 @@ public class Population {
 		
 		System.out.println(pop.en.getHm());
 		pop.sp.readFileParagraphs("book.txt");
+		//System.out.println(pop.sp.getSentencesList().get(0));
+		for(int i=0; i<pop.sp.getSentencesList().size();i++){
+			pop.displayWords(pop.sp.getSentencesList().get(i));
+		}
+		
 	}
-
+	
 }
