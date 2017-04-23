1c0d9e9a21a36d3931e458f0590804d328d82269
KarimGuesmi <karim_poste@yahoo.fr>
2017-04-22 21:38:48 -1200

    Update FitnessComputation.java

--- NewVersion/GeneticAlgorithm/FitnessComputation.java
+++ NewVersion/GeneticAlgorithm/FitnessComputation.java
@@ -25,68 +25,64 @@ public class FitnessComputation {
 	private List<String> listKeysWeight = new ArrayList<String>();
 	private Map<String, String> hmWeightKeys = new HashMap<>();
 	private List<Integer> fingers = new ArrayList<Integer>();
-	
-	// Lists of base Efforts for all keys => list of hand,finger and baselineEffort 
+
+	// Lists of base Efforts for all keys => list of hand,finger and
+	// baselineEffort
 	private List<String> keysFingers = new ArrayList<>();
-	private Map<String, String> keysBaseLineEffort = new HashMap<String,String>();
-	
+	private Map<String, String> keysBaseLineEffort = new HashMap<String, String>();
+
 	/*
-	 * Initialize the hash map of the key 's Base Line Effort
-	 * Each key ==> base line Effort(Double)
+	 * Initialize the hash map of the key 's Base Line Effort Each key ==> base
+	 * line Effort(Double)
 	 */
-	public void initializeKeyBaseLineEffort(){
+	public void initializeKeyBaseLineEffort() {
 		try {
 			Scanner scan1 = new Scanner(new File("keysWeight.txt"));
 			Scanner scan2 = new Scanner(new File("keysBaseLineEffort.txt"));
-			while (scan1.hasNext() && scan2.hasNext()){
-			    keysBaseLineEffort.put(scan1.next(), scan2.next());
+			while (scan1.hasNext() && scan2.hasNext()) {
+				keysBaseLineEffort.put(scan1.next(), scan2.next());
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/*
 	 * Initialize the list of finger's numbers for every Key
 	 */
-	public void initializeKeysFingers(){
+	public void initializeKeysFingers() {
 		try {
 			Scanner scan = new Scanner(new File("keysFingers.txt"));
-			while (scan.hasNext()){
-			    keysFingers.add(scan.next());
+			while (scan.hasNext()) {
+				keysFingers.add(scan.next());
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/*
-	 * This list is to calculate the penalties
-	 * Pij = W0+ Whand * Phand + Wrow * Prow + Wfinger * Pfinger
-	 * w0=0 and Phand=0 because we cannot penalize the left or right hand
-	 * The idea is to minimize the usage of the pinky and ring fingers 
-	 * and maximize the usage of the stronger fingers
-	 * avoiding the movement of both hand and focus more on the right hand
+	 * This list is to calculate the penalties Pij = W0+ Whand * Phand + Wrow *
+	 * Prow + Wfinger * Pfinger w0=0 and Phand=0 because we cannot penalize the
+	 * left or right hand The idea is to minimize the usage of the pinky and
+	 * ring fingers and maximize the usage of the stronger fingers avoiding the
+	 * movement of both hand and focus more on the right hand
 	 */
 	private List<Integer> penalties = new ArrayList<Integer>();
-	
-	
+
 	/*
-	 * The list of fingers number
-	 * The left hand is from 0 to 4
-	 * The right hand is from 5 to 9
-	 * We start counting from the Pinky
+	 * The list of fingers number The left hand is from 0 to 4 The right hand is
+	 * from 5 to 9 We start counting from the Pinky
 	 */
-	public void initializeFingers(){
-		for(int i=0; i<10;i++){
+	public void initializeFingers() {
+		for (int i = 0; i < 10; i++) {
 			fingers.add(i);
 		}
 	}
-	
+
 	public void fintnessComputationLists() {
 		try {
 			BufferedReader leftHand = new BufferedReader(new FileReader("listLeftHand.txt"));
@@ -96,8 +92,7 @@ public class FitnessComputation {
 			BufferedReader row3 = new BufferedReader(new FileReader("listRow3.txt"));
 			Scanner keys = new Scanner(new File("keysWeight.txt"));
 			Scanner keysWeight = new Scanner(new File("keysWeightValues.txt"));
-			
-			
+
 			String line1, line2, lineR1, lineR2, lineR3;
 
 			while ((line1 = leftHand.readLine()) != null) {
@@ -118,18 +113,17 @@ public class FitnessComputation {
 			while (keys.hasNext()) {
 				listKeys.add(keys.next());
 			}
-			
-			while(keysWeight.hasNext()){
+
+			while (keysWeight.hasNext()) {
 				listKeysWeight.add(keysWeight.next());
 			}
-			
+
 			leftHand.close();
 			rightHand.close();
 			row1.close();
 			row2.close();
 			row3.close();
-			
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -137,74 +131,88 @@ public class FitnessComputation {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		for(int i =0; i<listKeys.size();i++){
+
+		for (int i = 0; i < listKeys.size(); i++) {
 			hmWeightKeys.put(listKeys.get(i), listKeysWeight.get(i));
 		}
 
 	}
 
-	
 	/*
 	 * Compute Penalty of one stroke
 	 */
-	public double computePenalty(String stroke){
-		//http://mkweb.bcgsc.ca/carpalx/?typing_effort
-		int w0=0;
-		double row1 = 1.2; double row2 = 1.1 ; double row3 = 1;
-		
+	public double computePenalty(String stroke) {
+		// http://mkweb.bcgsc.ca/carpalx/?typing_effort
+		int w0 = 0;
+		double row1 = 1.2;
+		double row2 = 1.1;
+		double row3 = 1;
+
 		return 0;
-		
 	}
-	
+
 	/*
 	 * Penalty computation
 	 */
-	public double  strokePathEffort(String stroke){
-		double s=0;
+	public double strokePathEffort(String stroke) {
+		double s = 0;
 		// Weights of Hand, Row, Finger
-		int wH=0;
-		int wR=0;
-		int wF=0;
-		
-		
-		for(int i=0; i< stroke.length();i++){
-			
+		int wH = 0;
+		int wR = 0;
+		int wF = 0;
+
+		for (int i = 0; i < stroke.length(); i++) {
+
 		}
-		
+
 		return s;
 	}
-	
+
 	/*
-	 * Compute the effort of typing a Stroke
-	 * ei = kb * bi + kp * pi + ks * si
+	 * Compute the effort of typing a Stroke ei = kb * bi + kp * pi + ks * si
 	 * kb, kp and ks factors is to weight the indidividual effort components.
-	 * bi, pi and si are the base, penalty and stroke path effort components of the ith Stroke
+	 * bi, pi and si are the base, penalty and stroke path effort components of
+	 * the ith Stroke
 	 */
-	public double typingEffortOfStroke(String stroke){
-		//double bi = computeBaseOfStroke();
-		double bI = 0, pI=0, sI=0;
+	public double typingEffortOfStroke(String stroke) {
+		// double bi = computeBaseOfStroke();
+		double bI = 0, pI = 0, sI = 0;
 		int longStroke = stroke.length();
+		List<Double> baseAndWeight = new ArrayList<>();
 		
-		if (longStroke==1){
+		/*
+		 * bI computation
+		 */
+		if (longStroke == 0) {
+			bI=0;
+		} else if (longStroke == 1) {
 			bI = Double.parseDouble(hmWeightKeys.get(stroke)) * Double.parseDouble(keysBaseLineEffort.get(stroke));
+		} else if (longStroke == 2) {
+			bI = (Double.parseDouble(hmWeightKeys.get(stroke.charAt(0))) * Double.parseDouble(keysBaseLineEffort.get(stroke.charAt(0)))) 
+					+ (1 + (Double.parseDouble(hmWeightKeys.get(stroke.charAt(1))) * Double.parseDouble(keysBaseLineEffort.get(stroke.charAt(1)))));
+		}else if(longStroke>2){
+			for(int i=0; i< longStroke;i++){
+				baseAndWeight.add(Double.parseDouble(hmWeightKeys.get(stroke.charAt(i))) * Double.parseDouble(keysBaseLineEffort.get(stroke.charAt(i))));
+			}
+			double b=baseAndWeight.get(0);
+			for(int i=1; i<baseAndWeight.size();i++){
+				b = b * (1+baseAndWeight.get(i)) ;
+			}
+			bI = b;
 		}
-		
+
 		double eI = bI + pI + sI;
 		return eI;
 	}
-	
+
 	/*
-	 * Compute the typing Effort for the text 
-	 * It's given by this formula 
-	 * E = 1/N * Sum(ei)
-	 * N : Number of strokes 	;	ei : effort of typing a stroke
+	 * Compute the typing Effort for the text It's given by this formula E = 1/N
+	 * * Sum(ei) N : Number of strokes ; ei : effort of typing a stroke
 	 */
-	public double typingEffortOfText(int totalNumber, int sumEfforts){
-		return sumEfforts/totalNumber;
+	public double typingEffortOfText(int totalNumber, int sumEfforts) {
+		return sumEfforts / totalNumber;
 	}
-	
-	
+
 	/*
 	 * Main for TEST
 	 */
@@ -216,17 +224,17 @@ public class FitnessComputation {
 		System.out.println("Row 1 ==>  " + fit.listRow1);
 		System.out.println("Row 2 ==>  " + fit.listRow2);
 		System.out.println("Row 3 ==>  " + fit.listRow3);
-		System.out.println("List the keys : "+fit.listKeys);
-		System.out.println("List the keys Weight : "+fit.listKeysWeight);
-		System.out.println("Keys Weight ==> "+fit.hmWeightKeys);
+		System.out.println("List the keys : " + fit.listKeys);
+		System.out.println("List the keys Weight : " + fit.listKeysWeight);
+		System.out.println("Keys Weight ==> " + fit.hmWeightKeys);
 		fit.initializeFingers();
-		System.out.println("The list of fingers : "+fit.fingers);
-		
-		//Base effort Map
+		System.out.println("The list of fingers : " + fit.fingers);
+
+		// Base effort Map
 		fit.initializeKeysFingers();
 		System.out.println("* Key's Fingers :");
 		System.out.println(fit.keysFingers);
-		
+
 		// base line effort hash map
 		fit.initializeKeyBaseLineEffort();
 		System.out.println("* Key's base line effort");
