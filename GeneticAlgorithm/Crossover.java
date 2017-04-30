3af5b24d07b54c77b11ff43983bcfd13c496bc58
KarimGuesmi <karim_poste@yahoo.fr>
2017-04-30 03:11:51 -1200

    Update Crossover.java

--- GeneticAlgorithm/Crossover.java
+++ GeneticAlgorithm/Crossover.java
@@ -1,1 +1,20 @@
 
+package GeneticAlgorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Crossover {
+
+	private Entity entity;
+	private Map<String, String> entity1 = new HashMap<String,String>();
+	private Map<String, String> entity2 = new HashMap<String,String>();
+	
+	
+	
+	public static void main(String[] args) {
+		Crossover crossover = new Crossover();
+
+	}
+
+}
