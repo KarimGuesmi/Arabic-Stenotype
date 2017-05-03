28e970248e4b43ada90aef927f5d91bd23d97fad
KarimGuesmi <karim_poste@yahoo.fr>
2017-05-03 00:44:50 -1200

    Update Population.java

--- GeneticAlgorithm/Population.java
+++ GeneticAlgorithm/Population.java
@@ -2,6 +2,7 @@ package GeneticAlgorithm;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -13,7 +14,9 @@ public class Population {
 	private Random random = new Random();
 	private Crossover crossover;
 	private Mutation mutation;
-
+	private FitnessComputation fitness;
+	private List<Double>fitnessValues= new ArrayList<>();
+	
 	/*
 	 * Create a population
 	 */
@@ -22,15 +25,20 @@ public class Population {
 		for (int i = 0; i < populationSize; i++) {
 			entity = new Entity();
 			entities.add(entity.generateRandomEntities());
-			System.out.println("Random Entity " + i + " : ");
+			System.out.println("===> Random Entity " + i + " : ");
 			System.out.println(entities.get(i));
+			fitness = new FitnessComputation(entities.get(i));
+			System.out.println("** The fitness value of the Entity "+ i+" : "+fitness.getEffort());
+			fitnessValues.add(fitness.getEffort());
 		}
 	}
 
 	/*
 	 * Take two random entities and make a crossover between them
 	 */
-	public void doCrossover() throws IOException {
+	public void run() throws IOException {
+		// List of all fitness values
+		 
 		for (int i = 0; i < 10; i++) {
 			System.out.println(
 					"----------------------------------------------------------------------------------------------");
@@ -50,10 +58,37 @@ public class Population {
 			Mutation mutation1 = new Mutation(entity1);
 			System.out.println("** The Entity after the mutation");
 			System.out.println(mutation1.getFinalEntityMutated());
+			entities.set(index1, mutation1.getFinalEntityMutated());
+			
+			// Get the fitness value for all entities
+			fitness = new FitnessComputation(mutation1.getFinalEntityMutated());
+			System.out.println("** The fitness value of the Entity :"+index1+" : "+fitness.getEffort());
+			fitnessValues.set(index1, fitness.getEffort());
 			
 			
 		}
+		System.out.println("_____________________________________________");
+		System.out.println("** All Fitness values : ");
+		System.out.println(fitnessValues);
+		
+		// Select the highest fitness value
+		double highestFitness = fitnessValues.get(0) ;
+		for(int i=1; i< fitnessValues.size();i++){
+			if(fitnessValues.get(i)>highestFitness){
+				highestFitness = fitnessValues.get(i);
+			}
+		}
+		// Get the index of the highest Fitness Value
+		int indexHighestFitness = fitnessValues.indexOf(highestFitness);
+		System.out.println(indexHighestFitness);
+		
+		// Throw Away the corresponding entity 
+		// And generate a new Entity instead of it
 
+		// Generate a new Entity
+		Map<String,String> newEntity = new HashMap<String,String>();
+		newEntity = entity.generateRandomEntities();
+		entities.set(indexHighestFitness, newEntity);
 	}
 
 	/*
@@ -82,7 +117,7 @@ public class Population {
 
 		// do crossover for 5 entities
 		System.out.println("///////////////////////// THE CROSSOVER ///////////////////////////////");
-		pop.doCrossover();
+		pop.run();
 
 		// do mutation
 		//System.out.println("///////////////////////// THE MUTATION ///////////////////////////////");
