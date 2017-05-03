65c832fbd3b9e3fb5bc14b1bba6f766d3a36a048
KarimGuesmi <karim_poste@yahoo.fr>
2017-05-02 22:39:16 -1200

    Update FitnessComputation.java

--- GeneticAlgorithm/FitnessComputation.java
+++ GeneticAlgorithm/FitnessComputation.java
@@ -1,11 +1,46 @@
-
 package GeneticAlgorithm;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FitnessComputation {
+	
+	static Entity entite;
+	private static Map<String,String>entitee = new HashMap<String, String>();
+	
+	/*
+	 * Compute the fitness value from a given random generated entity
+	 */
+	public FitnessComputation(Map<String,String> entity) {
+		super();
+		double effort = computeEffort(entity); 
+		double penalty = computePenalty(entity);
+		
+		double fitness = effort - penalty;
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public double computePenalty(Map<String, String> entity) {
+		
+		return 0;
+	}
+
+
+	public double computeEffort(Map<String, String> entity) {
+		
+		return 0;
+	}
+
+
+	/*
+	 * Main Program for the TEST
+	 */
+	public static void main(String[] args) throws IOException {
+		entite = new Entity();
+		entitee = entite.generateRandomEntities();
+		FitnessComputation fitness = new FitnessComputation(entitee);
+		System.out.println(fitness);
 	}
 
 }
