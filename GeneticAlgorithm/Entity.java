15f0950d42f8d132f6901c46af7832a04a7411ff
KarimGuesmi <karim_poste@yahoo.fr>
2017-05-04 06:04:13 -1200

    Entity Modification

--- GeneticAlgorithm/Entity.java
+++ GeneticAlgorithm/Entity.java
@@ -20,15 +20,14 @@ import GeneticAlgorithmOld.RandomEntity;
  * All entities are represented as HashMap data structure Letter(Arabic) ==> Key(Stenotype)
  */
 public class Entity {
-	
 	/*
 	 * Base data structure of the Entity
 	 */
 	private Random random = new Random();
 	private int entityLength = 5;
-	
+
 	private int fitness = 0;
-	
+
 	// Additional tools to save our data
 	private List<String> listLetters = new ArrayList<String>();
 	private List<String> listKeys = new ArrayList<String>();
@@ -36,7 +35,7 @@ public class Entity {
 
 	private Map<String, String> hmEntity = new HashMap<>();
 	private Map<String, String> hmRandomEntity = new HashMap<>();
-	
+
 	/*
 	 * Create A Random Individual
 	 */
@@ -46,52 +45,51 @@ public class Entity {
 		defaultEntityHashMap(listLetters, listKeys);
 	}
 
-	
 	/*
 	 * Create a default hash map from the default two lists keys and letters
 	 */
 	private void defaultEntityHashMap(List<String> listLetters2, List<String> listKeys2) {
 		for (int i = 0; i < listLetters.size(); i++) {
 			hmEntity.put(listLetters.get(i), listKeys.get(i));
-		}	
+		}
 	}
 
-	
 	/*
-	 * Default entity by using a default list of letters and keys without Random java class
+	 * Default entity by using a default list of letters and keys without Random
+	 * java class
 	 */
 	private void defalutEntityLists(String letterFile, String keyFile) throws IOException {
 		// Create List of Arabic Letters
-				BufferedReader reader1 = new BufferedReader(new FileReader(letterFile));
-				String line1;
-				while ((line1 = reader1.readLine()) != null) {
-					listLetters.add(line1);
-				}
-				reader1.close();
+		BufferedReader reader1 = new BufferedReader(new FileReader(letterFile));
+		String line1;
+		while ((line1 = reader1.readLine()) != null) {
+			listLetters.add(line1);
+		}
+		reader1.close();
 
-				// Create List of Keys
-				BufferedReader reader2 = new BufferedReader(new FileReader(keyFile));
-				String line2;
-				while ((line2 = reader2.readLine()) != null) {
-					listKeys.add(line2);
-				}
-				reader2.close();
+		// Create List of Keys
+		BufferedReader reader2 = new BufferedReader(new FileReader(keyFile));
+		String line2;
+		while ((line2 = reader2.readLine()) != null) {
+			listKeys.add(line2);
+		}
+		reader2.close();
 	}
 
 	/*
-	 * Generate some random entities
-	 * In our case  Random entities should be created and putted into a hashMap data structure
+	 * Generate some random entities In our case Random entities should be
+	 * created and putted into a hashMap data structure
 	 */
-	public Map<String,String>  generateRandomEntities() {
-		//for(int i=0; i<entityLength;i++){
-			randomEntityKeys(listKeys);
-			
-			//System.out.println(listKeysRandom);
-			randomHashMap(listKeysRandom);
-			//System.out.println(hmRandomEntity);
-			listKeysRandom.clear();
-		//}
-			return hmRandomEntity;
+	public Map<String, String> generateRandomEntities() {
+		// for(int i=0; i<entityLength;i++){
+		randomEntityKeys(listKeys);
+
+		// System.out.println(listKeysRandom);
+		randomHashMap(listKeysRandom);
+		// System.out.println(hmRandomEntity);
+		listKeysRandom.clear();
+		// }
+		return hmRandomEntity;
 	}
 
 	/*
@@ -101,9 +99,13 @@ public class Entity {
 		for (int i = 0; i < listKeysRandom.size(); i++) {
 			hmRandomEntity.put(listLetters.get(i), listKeysRandom.get(i));
 		}
-		
 	}
-
+	
+	/*
+	 * Generate randomly the list of keys in different order 
+	 * This will serve lately for the last HashMap
+	 * Where we will put all the keys to its corresponding Arabic letter 
+	 */
 	public void randomEntityKeys(List<String> listKeys2) {
 		int index;
 		while (listKeysRandom.size() != listKeys.size()) {
@@ -114,26 +116,22 @@ public class Entity {
 				}
 			}
 		}
-		
 	}
 
-	
 	/*
 	 * Main Program for a test
 	 */
 	public static void main(String[] args) throws IOException {
 		Entity en = new Entity();
 		System.out.println("The Default Entity");
-		//System.out.println(en.listKeys);
-		//System.out.println(en.listLetters);
+		// System.out.println(en.listKeys);
+		// System.out.println(en.listLetters);
 		System.out.println(en.hmEntity);
 		System.out.println("____________________________________________________________");
-		
+
 		// Generate some random entities
 		System.out.println("The Random Entity : ");
 		System.out.println(en.generateRandomEntities());
 		System.out.println("____________________________________________________________");
-		
 	}
-
 }
