6a79cf8c5871a3581620bbba4ca58ccb788ab295
KarimGuesmi <karim_poste@yahoo.fr>
2017-05-11 21:51:55 -1200

    Outline.java : NullPointerException Resolving

--- GeneticAlgorithm/Outline.java
+++ GeneticAlgorithm/Outline.java
@@ -19,140 +19,142 @@ import org.json.simple.JSONObject;
 import preparation.SplitingText;
 
 public class Outline {
-	
+
 	private static Algorithm algo;
 	private SplitingText words = new SplitingText();
-	
+
 	public void createOutlineFile(String file, List<String> strokeDictionaryImproved) {
-		try{
-		    PrintWriter writer = new PrintWriter(file, "UTF-8");
-		    for(int i=0; i<strokeDictionaryImproved.size();i++){
-		    	writer.println(strokeDictionaryImproved.get(i));
-		    }
-		    writer.close();
+		try {
+			PrintWriter writer = new PrintWriter(file, "UTF-8");
+			for (int i = 0; i < strokeDictionaryImproved.size(); i++) {
+				writer.println(strokeDictionaryImproved.get(i));
+			}
+			writer.close();
 		} catch (IOException e) {
 		}
 	}
-	
+
 	/*
 	 * 
 	 */
 	/*
-	public void createJSONOutline(String outline, String dictionary, String jsonDict) throws IOException {
-		JSONObject json = new JSONObject();
-		
-		FileInputStream fis1 =new FileInputStream(dictionary);
-		BufferedReader br1 = new BufferedReader(new InputStreamReader(fis1));
-		
-		FileInputStream fis2 =new FileInputStream(outline);
-		BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
-		
-		String strLine1;
-		String strLine2;
-		
-		while(((strLine1 = br1.readLine()) != null) && ((strLine2 = br2.readLine()) != null)){
-			json.put(strLine1, strLine2);
-		}
-		
-		FileWriter fw = new FileWriter(jsonDict);
-		fw.write(json.toJSONString());
-		
-		fw.flush();
-		fw.close();
-	}
-	*/
-	
+	 * public void createJSONOutline(String outline, String dictionary, String
+	 * jsonDict) throws IOException { JSONObject json = new JSONObject();
+	 * 
+	 * FileInputStream fis1 =new FileInputStream(dictionary); BufferedReader br1
+	 * = new BufferedReader(new InputStreamReader(fis1));
+	 * 
+	 * FileInputStream fis2 =new FileInputStream(outline); BufferedReader br2 =
+	 * new BufferedReader(new InputStreamReader(fis2));
+	 * 
+	 * String strLine1; String strLine2;
+	 * 
+	 * while(((strLine1 = br1.readLine()) != null) && ((strLine2 =
+	 * br2.readLine()) != null)){ json.put(strLine1, strLine2); }
+	 * 
+	 * FileWriter fw = new FileWriter(jsonDict); fw.write(json.toJSONString());
+	 * 
+	 * fw.flush(); fw.close(); }
+	 */
+
 	private void createJSONOutline(String outline, String dictionary, String jsonDic) throws IOException {
-		FileInputStream fis1 =new FileInputStream(dictionary);
+		FileInputStream fis1 = new FileInputStream(dictionary);
 		BufferedReader br1 = new BufferedReader(new InputStreamReader(fis1));
-		
-		FileInputStream fis2 =new FileInputStream(outline);
+
+		FileInputStream fis2 = new FileInputStream(outline);
 		BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
-		
+
 		String strLine1;
 		String strLine2;
-		
-		//FileWriter fw = new FileWriter(jsonDic);
+
+		// FileWriter fw = new FileWriter(jsonDic);
 		FileOutputStream fos = new FileOutputStream(jsonDic);
 		BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(fos));
 		fw.write("{");
-		while(((strLine1 = br1.readLine()) != null) && ((strLine2 = br2.readLine()) != null)){
-			fw.write("\"");fw.write(strLine2);fw.write("\"");
+		while (((strLine1 = br1.readLine()) != null) && ((strLine2 = br2.readLine()) != null)) {
+			fw.write("\"");
+			fw.write(strLine2);
+			fw.write("\"");
 			fw.write(":");
-			fw.write("\"");fw.write(strLine1);fw.write("\"");
-			fw.write(",");fw.newLine();
+			fw.write("\"");
+			fw.write(strLine1);
+			fw.write("\"");
+			fw.write(",");
+			fw.newLine();
 		}
 		// to avoid the last "," in the last line in the file
-		fw.write("\"");fw.write("رررر");fw.write("\"");fw.write(":");
-		fw.write("\"");fw.write("STKPWHRAO");fw.write("\"");
+		fw.write("\"");
+		fw.write("رررر");
+		fw.write("\"");
+		fw.write(":");
+		fw.write("\"");
+		fw.write("STKPWHRAO");
+		fw.write("\"");
 		fw.newLine();
-		
+
 		fw.write("}");
 		fw.close();
-		
+
 	}
-	
+
 	/*
 	 * Main Program for the Text
 	 */
-	
+
 	public static void main(String[] args) throws IOException {
 		Outline out = new Outline();
 		algo = new Algorithm("dictionary.txt");
-		//System.out.println(algo.getStrokeDictionaryImproved());
+		// System.out.println(algo.getStrokeDictionaryImproved());
 		out.createOutlineFile("outline.txt", algo.getStrokeDictionaryImproved());
-		
-		// Create A JSON FILE Containing all the words and it's corresponding strokes
+
+		// Create A JSON FILE Containing all the words and it's corresponding
+		// strokes
 		out.createJSONOutline("outline.txt", "dictionary.txt", "jsonDictionary.json");
-		
-		// Use the splittingWords class, get the text and the words ==> 
+
+		// Use the splittingWords class, get the text and the words ==>
 		// The translate the text into words
 		out.words.readFileParagraphs("bookk.txt");
 		out.words.paragIntoSentences();
 		out.words.splitWords();
-		//System.out.println(out.words.getWordsList());
-		
+		// System.out.println(out.words.getWordsList());
+
 		// Insert all words into a file
 		out.insetWords(out.words.getWordsList());
 
-		//Translate the text into strokes
-		List<String>strokes = out.translateTextIntoStrokes(out.words.getWordsList());
+		// Translate the text into strokes
+		List<String> strokes = out.translateTextIntoStrokes(out.words.getWordsList());
 		out.createOutlineFile("translatedText.txt", strokes);
-		
+
 		// Save the best entity into a file
-				try (
-					FileOutputStream fos = new FileOutputStream("bestEntity.txt");
-					PrintWriter pw = new PrintWriter(new OutputStreamWriter(fos));
-				) {
-					pw.println(algo.getBestEntity());
-					pw.flush();
-				}
+		try (FileOutputStream fos = new FileOutputStream("bestEntity.txt");
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(fos));) {
+			pw.println(algo.getBestEntity());
+			pw.flush();
+		}
 	}
 
 	private void insetWords(ArrayList<String> wordsList) throws IOException {
 		File fout = new File("wordsFromText.txt");
 		FileOutputStream fos = new FileOutputStream(fout);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-		
-		for(int i=0; i<wordsList.size();i++){
-			if(wordsList.get(i)!=null){
+
+		for (int i = 0; i < wordsList.size(); i++) {
+			if (wordsList.get(i) != null) {
 				bw.write(wordsList.get(i));
 				bw.newLine();
 			}
 		}
-		
+
 	}
 
 	private List<String> translateTextIntoStrokes(ArrayList<String> words) throws IOException {
 		Algorithm algoText = new Algorithm("wordsFromText.txt", algo.getBestEntity());
-		
+
 		List<String> strokes = new ArrayList<>();
-		
+
 		strokes = algoText.getStrokeDictionaryImproved();
-		
+
 		return strokes;
 	}
-
-	
 
 }
